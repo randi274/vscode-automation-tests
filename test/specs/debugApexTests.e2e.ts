4c1a49ef5453dd17c0ea0bb816ffb474b0c84994
Randi Wilson <randi274@gmail.com>
2023-02-02 13:57:09 -0500

    initial commit

---
+++ test/specs/debugApexTests.e2e.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { step } from 'mocha-steps';
+import {
+  ScratchOrg
+} from '../scratchOrg';
+
+describe('Debug Apex Tests', async () => {
+  let scratchOrg: ScratchOrg = undefined;
+
+  step('Set up the testing environment', async () => {
+    scratchOrg = new ScratchOrg('DebugApexTests', false);
+    await scratchOrg.setUp();
+  });
+
+  step('Enable the Apex debugger', async () => {
+    // Open the command palette
+
+    // type in "SFDX: Turn Apex debugging on"
+
+    // press return
+
+    // wait 10 seconds
+
+    // etc...
+
+    // etc...
+
+    // etc...
+
+    expect(1).toBe(1);
+  });
+
+  step('what?', async () => {
+    // TODO: implement
+    expect(1).toBe(1);
+  });
+
+  step('Tear down and clean up the testing environment', async () => {
+    await scratchOrg.tearDown();
+  });
+});
---
+++ test/specs/functions.e2e.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { step } from 'mocha-steps';
+import {
+  ScratchOrg
+} from '../scratchOrg';
+
+describe('Functions', async () => {
+  let scratchOrg: ScratchOrg;
+
+  step('Set up the testing environment', async () => {
+    scratchOrg = new ScratchOrg('Functions', false);
+    await scratchOrg.setUp();
+  });
+
+  // Java Functions
+    step('Create a Java function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start Java function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop Java function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start Java function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop Java function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+  // JavaScript Functions
+    step('Create a JavaScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start JavaScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop JavaScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start JavaScript function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop JavaScript function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+  // TypeScript Functions
+    step('Create a TypeScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start TypeScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop TypeScript function', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Start TypeScript function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+    step('Stop TypeScript function in container', async () => {
+      // TODO: implement
+      expect(1).toBe(1);
+    });
+
+  step('Tear down and clean up the testing environment', async () => {
+    await scratchOrg.tearDown();
+  });
+});
